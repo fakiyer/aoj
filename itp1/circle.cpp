10b20f69687e34ef17fe4e5b0aa6bb7832923731
fakiyer <fakiyer@gmail.com>
2020-03-05 21:23:08 +0900

    ITP1_4_B

---
+++ itp1/circle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <stdio.h>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+  double r;
+
+  cin >> r;
+
+  printf("%f %f\n", r * r * M_PI, 2 * r * M_PI);
+
+  return 0;
+}
